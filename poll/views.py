63e24d8bb3abe87e2374d86c665a2c3b733d45b1
sean.li <bj_cctv@hotmail.com>
2015-01-29 17:13:21 +0800

    tongyong_shitu

--- poll/views.py
+++ poll/views.py
@@ -5,9 +5,11 @@ from poll.models import Poll,Choice
 from django.template import Context,loader
 from django.template import RequestContext
 from django.core.urlresolvers import reverse,resolve
+from django.views import generic
 
 
 
+"""
 # 因为表单使用的是post方法，所以每个view 后都添加了 context_instance=RequestContext(request)
 #否则可能会出现这个错误: {% csrf_token %} was used in a template, but the context did not provide the value. 
 # Create your views he
@@ -23,19 +25,6 @@ def index(req):
 	return render_to_response('index.html',{'poll_list':poll_list},context_instance=RequestContext(req))
 
 
-def vote(req,poll_id):
-	p = get_object_or_404(Poll,pk=poll_id)
-	try:
-		selected_choice=p.choice_set.get(pk=req.POST['choice'])
-	except(KeyError,Choice.DoesNotExist):
-		return render(req,'poll/detail.html',{'error_message':'No Choice'})
-	else:
-		selected_choice.votes+=1
-		selected_choice.save()
-		# reverse:通过传递给Reverse函数相应的url_name以及必要的参数，那么便会生成相应的url
-		#此处的polls:result表示polls域名空间下的name=result的url
-		return HttpResponseRedirect(reverse('polls:result',args=(p.id,)))
-
 def detail(req,poll_id):
 
 	try:
@@ -50,9 +39,42 @@ def detail(req,poll_id):
 	poll = get_object_or_404(Poll,pk=poll_id)
 	'''
 	return render_to_response('detail.html',{'poll':poll},context_instance=RequestContext(req))
-
+	
 def result(req,poll_id):
 	poll = get_object_or_404(Poll,pk=poll_id)
 	return render_to_response('result.html',{'poll':poll},context_instance=RequestContext(req))
+"""
+
+#通用视图
+
+class IndexView(generic.ListView):
+	template_name = 'index.html'
+	context_object_name = 'poll_list'
+	def get_queryset(self):
+		return Poll.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Poll
+	template_name = 'detail.html'
+	
+
+class ResultView(generic.ListView):
+	model = Poll
+	template_name = 'result.html'
+	
+def vote(req,poll_id):
+	p = get_object_or_404(Poll,pk=poll_id)
+	try:
+		selected_choice=p.choice_set.get(pk=req.POST['choice'])
+	except(KeyError,Choice.DoesNotExist):
+		return render(req,'poll/detail.html',{'error_message':'No Choice'})
+	else:
+		selected_choice.votes+=1
+		selected_choice.save()
+		# reverse:通过传递给Reverse函数相应的url_name以及必要的参数，那么便会生成相应的url
+		#此处的polls:result表示polls域名空间下的name=result的url
+		return HttpResponseRedirect(reverse('polls:result',args=(p.id,)))
+
+
 
 
